93b2b6de8b50323f79da2d73520c5dc26f405142
Bram Lobbens <lobbensbram@gmail.com>
2018-10-25 14:54:12 +0200

    add: oefening 10

---
+++ programmeren1/extra/oefeningen/Klinker.java
@@ -0,0 +1,16 @@
+/*
+Oefening 10
+*/
+import java.util.Scanner;
+
+public class Klinker {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Geef een letter: ");
+        char letter = (reader.nextLine().toCharArray())[0];
+        boolean isVowel = !("aiueo".indexOf(letter) < 0);
+        System.out.printf("Het karakter %c is %s klinker.\n", letter, isVowel ? "een" : "geen");
+    }
+}
